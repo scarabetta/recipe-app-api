4efcc78947c491f673d7214e2b0dc8d01c5bf3ab
Sergio Carabetta <scarabetta@gmail.com>
2025-11-02 23:36:41 -0300

    Fix lint issues

--- app/app/test.py
+++ app/app/test.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -20,3 +21,4 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
+
